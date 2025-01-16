4bb2ff50ccf213ba4b95a8fcfae8c7cd59ecc773
Thiago César <thiago.or.code@gmail.com>
2025-01-16 18:18:29 -0300

    up

--- resources/views/app/Sale/manager.blade.php
+++ resources/views/app/Sale/manager.blade.php
@@ -216,7 +216,7 @@
                                                 <input type="hidden" name="id" value="{{ $sale->id }}"> 
                                                 <div class="btn-group" role="group" aria-label="Basic outlined example">
                                                     <a title="Faturas" href="{{ route('update-sale', ['id' => $sale->id]) }}" class="btn btn-outline-primary"><i class="bi bi-currency-dollar"></i></a>
-                                                    @if ($sale->status == 1 && Auth::user()->type == 1)
+                                                    @if ($sale->status == 1 && (Auth::user()->type == 1 || Auth::user()->level == 4 || Auth::user()->level == 5))
                                                         <a title="Reprotocolar" href="{{ route('reprotocol-sale', ['id' => $sale->id]) }}" class="btn btn-outline-primary"><i class="bx bx-check-shield"></i></a>
                                                     @endif
                                                     <button type="submit" class="btn btn-danger text-light"><i class="bi bi-trash"></i></button>
